6191c655e60f5ae7780e27c9c0612757b209c918
Zander Keith <zkeith@mit.edu>
2024-11-09 15:34:30 -0500

    Set up saving the results of the pitch check to determine ball's velocity near the plate.

--- iiwa_batter/sandbox/pitch_check.py
+++ iiwa_batter/sandbox/pitch_check.py
@@ -10,13 +10,15 @@ from iiwa_batter.physics import feet_to_meters, mph_to_mps
 
 def make_model_directive(initial_joint_positions, dt=DEFAULT_TIMESTEP):
     # We're pitchin the ball from +x to -x
-    # The robot is sitting at the origin because I ain't messing with blender
+    # The robot is sitting next to the origin
 
-    plate_offset_y = -0.8
+    plate_offset_y = 0.8
     pitch_start_x = feet_to_meters(60.5) # Pitcher's mound is 60.5 feet from home plate
     pitch_start_z = feet_to_meters(5.9)
 
     strike_zone_z = 0.6
+
+    base_rotation_deg = np.rad2deg(initial_joint_positions[0])
 
     model_directive = f"""
 directives:
@@ -24,16 +26,20 @@ directives:
     name: iiwa
     file: file://{PACKAGE_ROOT}/assets/iiwa14.urdf
     default_joint_positions:
-        iiwa_joint_1: [{initial_joint_positions[0]}]
-        iiwa_joint_2: [{initial_joint_positions[1]}]
-        iiwa_joint_3: [{initial_joint_positions[2]}]
-        iiwa_joint_4: [{initial_joint_positions[3]}]
-        iiwa_joint_5: [{initial_joint_positions[4]}]
-        iiwa_joint_6: [{initial_joint_positions[5]}]
-        iiwa_joint_7: [{initial_joint_positions[6]}]
+        iiwa_joint_1: [{initial_joint_positions[1]}]
+        iiwa_joint_2: [{initial_joint_positions[2]}]
+        iiwa_joint_3: [{initial_joint_positions[3]}]
+        iiwa_joint_4: [{initial_joint_positions[4]}]
+        iiwa_joint_5: [{initial_joint_positions[5]}]
+        iiwa_joint_6: [{initial_joint_positions[6]}]
+        iiwa_joint_7: [{initial_joint_positions[7]}]
 - add_weld:
     parent: world
     child: iiwa::base
+    X_PC:
+        rotation: !Rpy
+            deg: [0, 0, {base_rotation_deg}]
+        translation: [0, {plate_offset_y}, 0]
 - add_model:
     name: bat
     file: file://{PACKAGE_ROOT}/assets/bat.sdf
@@ -55,7 +61,7 @@ directives:
     file: file://{PACKAGE_ROOT}/assets/ball.sdf
     default_free_body_pose:
         base:
-            translation: [{pitch_start_x}, {plate_offset_y}, {pitch_start_z}]
+            translation: [{pitch_start_x}, 0, {pitch_start_z}]
 - add_model:
     name: strike_zone
     file: file://{PACKAGE_ROOT}/assets/strike_zone.sdf
@@ -63,7 +69,7 @@ directives:
     parent: world
     child: strike_zone::base
     X_PC:
-        translation: [0, {plate_offset_y}, {strike_zone_z}]
+        translation: [0, 0, {strike_zone_z}]
 
 model_drivers:
     iiwa: !IiwaDriver
@@ -79,13 +85,13 @@ plant_config:
 
     return model_directive
 
-def run_pitch_check(meshcat, record_time, pitch_velocity_mph=90, save_time = dt=DEFAULT_TIMESTEP):
+def run_pitch_check(meshcat, record_time, pitch_velocity_mph=90, save_time=0.45, dt=DEFAULT_TIMESTEP):
     if meshcat is not None:
         meshcat.Delete()
 
     builder = DiagramBuilder()
 
-    model_directive = make_model_directive(joint_positions, dt)
+    model_directive = make_model_directive([0]*8, dt)
 
     scenario = LoadScenario(data=model_directive)
     station = builder.AddSystem(MakeHardwareStation(scenario, meshcat))
@@ -108,13 +114,35 @@ def run_pitch_check(meshcat, record_time, pitch_velocity_mph=90, save_time = dt=
     if meshcat is not None:
         meshcat.StartRecording()
 
-    times = np.linspace(0, record_time, int(record_time / dt))
-    ball_x_positions = np.zeros_like(times)
-    ball_x_velocities = np.zeros_like(times)
-    for i, time in enumerate(times):
-        simulator.AdvanceTo(time)
-        ball_x_positions[i] = plant.GetPositions(plant_context, ball)[4]
-        ball_x_velocities[i] = plant.GetVelocities(plant_context, ball)[3]
+    simulator.AdvanceTo(save_time)
+    ball_position = plant.GetPositions(plant_context, ball)
+    ball_velocity = plant.GetVelocities(plant_context, ball)
+    simulator.AdvanceTo(record_time)
 
     if meshcat is not None:
         meshcat.PublishRecording()
+
+    ball_state = {
+        "time": save_time,
+        "position":
+            {
+                "q0": ball_position[0],
+                "q1": ball_position[1],
+                "q2": ball_position[2],
+                "q3": ball_position[3],
+                "x": ball_position[4],
+                "y": ball_position[5],
+                "z": ball_position[6],
+            },
+        "velocity":
+            {
+                "rx": ball_velocity[0],
+                "ry": ball_velocity[1],
+                "rz": ball_velocity[2],
+                "vx": ball_velocity[3],
+                "vy": ball_velocity[4],
+                "vz": ball_velocity[5],
+            }
+    }
+
+    return ball_state
---
+++ iiwa_batter/swing_optimization/instantaneous_swing.py
@@ -0,0 +1,61 @@
+import numpy as np
+from manipulation.station import LoadScenario, MakeHardwareStation
+from manipulation.utils import RenderDiagram
+from pydrake.all import (
+    DiagramBuilder,
+    Simulator,
+)
+
+from iiwa_batter import PACKAGE_ROOT, DEFAULT_TIMESTEP
+from iiwa_batter.sandbox.pitch_check import make_model_directive
+
+def loss_function():
+    return 0
+
+def run_instantaneous_swing(meshcat, plate_joint_positions, plate_joint_velocities, plate_ball_state_arrays:tuple[np.ndarray, np.ndarray], dt=DEFAULT_TIMESTEP):
+    if meshcat is not None:
+        meshcat.Delete()
+
+    builder = DiagramBuilder()
+
+    model_directive = make_model_directive(plate_joint_positions, dt)
+
+    scenario = LoadScenario(data=model_directive)
+    station = builder.AddSystem(MakeHardwareStation(scenario, meshcat))
+    diagram = builder.Build()
+    simulator = Simulator(diagram)
+    context = simulator.get_context()
+
+    plant = station.GetSubsystemByName("plant")
+    plant_context = plant.GetMyContextFromRoot(context)
+
+    # Get the position of the sweet spot
+    sweet_spot = plant.GetModelInstanceByName("sweet_spot")
+    sweet_spot_position = plant.GetPositions(plant_context, sweet_spot)
+    # If the sweet spot is too far from the ball's position when it crosses the strike zone, stop here and return cost function
+
+    # The bat is in a position where it has a chance to hit the ball
+    # Ensure there are no self-collisions. If there are, return some metric of how bad the self-collision is
+
+    # Set the the ball's position and velocity at home plate, then execute the swing
+    # We're starting at 0.45 seconds and going to 0.5 seconds
+    ball = plant.GetModelInstanceByName("ball")
+    plant.SetPositions(plant_context, ball, plate_ball_state_arrays[0])
+    plant.SetVelocities(plant_context, ball, plate_ball_state_arrays[1])
+
+    # Set the joint velocities of the iiwa
+    iiwa = plant.GetModelInstanceByName("iiwa")
+    plant.SetVelocities(plant_context, iiwa, plate_joint_velocities)
+
+    # Just turn off the torque for the time being
+    station_context = station.GetMyContextFromRoot(context)
+    station.GetInputPort("iiwa.torque").FixValue(station_context, [0]*7)
+
+    # If the ball is traveling forwards, get how far it flies.
+    if meshcat is not None:
+        meshcat.StartRecording()
+
+    simulator.AdvanceTo(0.05)
+
+    if meshcat is not None:
+        meshcat.PublishRecording()
